b8773cefe9be16d0b999430698888fdce6264107
Lemonatix <ph.mika.ma.riesterer@gmail.com>
2024-12-29 00:55:33 +0100

    adds comment regarding tensorflow cudo error in nn_with_tensorflow_keras.py

--- nn_with_tensorflow_keras.py
+++ nn_with_tensorflow_keras.py
@@ -39,3 +39,9 @@ print(f"Test Loss: {loss:.4f}, Test MAE: {mae:.4f}")
 predictions = model.predict(x_test)
 for x_val, pred_val, true_val in zip(x_test[:5], predictions[:5], y_test[:5]):
     print(f"x={x_val:.2f} | Predicted={pred_val[0]:.2f} | Actual={true_val:.2f}")
+
+'''
+if you don't use a graphics card with cuda cores to run the file, you will get an error message:
+    to rid the error, do pip install tensorflow-cpu, but this is optional
+    the file itself runs normal without the install
+'''
